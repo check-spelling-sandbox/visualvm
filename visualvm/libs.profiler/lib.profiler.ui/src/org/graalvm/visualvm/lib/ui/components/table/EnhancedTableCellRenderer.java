658dbb8bd575c96a3516f08e359f7852c549e521
Josh Soref <jsoref@users.noreply.github.com>
2020-09-10 23:34:45 -0400

    spelling: alternating
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- visualvm/libs.profiler/lib.profiler.ui/src/org/graalvm/visualvm/lib/ui/components/table/EnhancedTableCellRenderer.java
+++ visualvm/libs.profiler/lib.profiler.ui/src/org/graalvm/visualvm/lib/ui/components/table/EnhancedTableCellRenderer.java
@@ -32,7 +32,7 @@ import org.graalvm.visualvm.lib.ui.UIUtils;
 
 
 /** An abstract superclass for table cell renderers to be used throughout the profiler.
- * It implements alrenating background colors for odd and even rows, and simplifies the writing of
+ * It implements alternating background colors for odd and even rows, and simplifies the writing of
  * concrete renderers by masking Swing's ugly CellRenderer API (by forcing "this" to be returned as the renderer).
  *
  * @author Ian Formanek
